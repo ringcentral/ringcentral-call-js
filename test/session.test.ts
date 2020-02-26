92dadc8ada5080d7e6c9b9a72e806e47e7e87969
embbnux <embbnux@embbnux.com>
2020-02-26 16:18:42 +0800

    tests: add test

--- test/session.test.ts
+++ test/session.test.ts
@@ -1,12 +1,21 @@
 import { Session } from '../src/Session';
 import { WebPhoneSession } from './__mocks__/ringcentral-web-phone/lib/Session';
+import { Session as TelephonySession } from './__mocks__/ringcentral-call-control/lib/Session';
 
 describe('RingCentral Call :: Session ::', () => {
-  test('it should be initialzed', () => {
+  test('it should be initialzed with web phone session', () => {
     const webphoneSession = new WebPhoneSession({ id: '123', fromNumber: '101', toNumber: '102' });
     const session = new Session({
       webphoneSession,
     });
     expect(session.webphoneSession).toEqual(webphoneSession);
+  });
+
+  test('it should be initialzed with telephony session', () => {
+    const telephonySession = new TelephonySession({ id: '123', fromNumber: '101', toNumber: '102' });
+    const session = new Session({
+      telephonySession,
+    });
+    expect(session.telephonySession).toEqual(telephonySession);
   });
 });
